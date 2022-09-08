98e2442b532c9aa617335fb35c574d16946a1904
nicolasnetz <nicolasnetz@github.com>
2022-09-08 14:18:51 -0400

    includes iso_3

--- etl/dim_countries.py
+++ etl/dim_countries.py
@@ -12,20 +12,18 @@ class CountriesStep(PipelineStep):
     def run_step(self, prev_result, params):
         
         #  logger.info("Downloading Members: {} {}  from OEC...".format(payload['cube'], payload['level']))
-
         oec = OEC()
+        payload = {
+            'cube': 'trade_i_baci_a_92',
+            'level': 'Country'
+        }
+        df = oec.get_members(payload=payload)
 
-        cube = 'trade_i_baci_a_92'
-        drilldown = ['Exporter Country']
-        measure = ['Trade Value']
-        properties = ['Exporter Country ISO 3']
+        df['iso_3'] = df['id'].str.slice(start=2).str.upper()
 
-        df = oec.get_data(auth=True, cube=cube,drilldown=drilldown, measure=measure, properties=properties,token='560ac5684ded4f8d63074ed94739772b')
-
-        df['iso_3'] = df['iso_3'].str.upper()
-        
-        df = df[['country_id','country', 'iso_3']]
+        df = df[['id','label', 'iso_3']]
         df.columns = ['oec_id','comtrade_name', 'iso_3']
+
         return df
 
 
