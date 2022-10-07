57303e394f10c46a046f8f6315d65d02807b62ca
nicolasnetz <nicolasnetz@github.com>
2022-10-07 11:01:46 -0300

    update oec module

--- etl/modules/oec.py
+++ etl/modules/oec.py
@@ -3,7 +3,7 @@ import os
 import requests
 import pandas as pd
 
-from typing import Optional
+from time import sleep
 
 class OEC:
     def __init__(self) -> None:
@@ -18,14 +18,25 @@ class OEC:
         }
         """
         base_url = 'https://oec.world/olap-proxy/members?'
-        r = requests.get(base_url, params = payload)
-        print(r.url)
+
+        while True:    
+            try: 
+                r = requests.get(base_url, params = payload)
+                # Print requested url
+                print(r.url)
+            except:
+                print('Error performing request {}'.format(r.status_code))
+                sleep(10)
+            else:
+                print('Successful data call to OEC {}'.format(r.status_code))
+                break
+
         df = pd.DataFrame(r.json()['data'])
         df.sort_values('ID').reset_index(drop=True)
         df.columns = df.columns.map(lambda x: x.replace(' ', '_').lower())
         return df
 
-    def get_data(self, auth:bool, cube:str, drilldown:list, measure:list,token:Optional[str], cut=None, properties=None):
+    def get_data(self, auth:bool, cube:str, drilldown:list, measure:list, cut:dict, properties:dict=None, token:str = ''):
         """
         usage example:
         cut = {
@@ -36,16 +47,20 @@ class OEC:
         measure = ['Trade Value']
         cube='trade_i_baci_a_92'
         token = 'my_token'
-                
-        """
+
+        oec.get_data(False, cube, drlldown, measure, cut)
         
+        """
+        base_url = 'https://oec.world/olap-proxy/data.jsonrecords?'
+
         if cut == None:
             payload = {}
         else:
             payload = cut.copy()
-
+        
         drilldown = ','.join(drilldown)
         measure = ','.join(measure)
+
         payload['cube'] = cube
         payload['drilldowns'] = drilldown
         payload['measures'] = measure
@@ -53,14 +68,26 @@ class OEC:
         if properties != None:
             payload['properties'] = properties
 
-        base_url = 'https://oec.world/olap-proxy/data.jsonrecords?'
-
         if auth:
             payload['token'] = token if token else os.environ['OEC_TOKEN']
-            
-        r = requests.get(base_url, params = payload)
-        # print(r.url)
+        
+        while True:    
+            try: 
+                r = requests.get(base_url, params = payload)
+                # Print requested url
+                print('Request:')
+                print(r.url)
+            except:
+                print('Error performing request {}'.format(r.status_code))
+                sleep(10)
+            else:
+                print('Successful data call to OEC {}'.format(r.status_code))
+                break
         df = pd.DataFrame(r.json()['data'])
-        df.columns = df.columns.map(lambda x: x.replace(' ', '_').lower())
+        
+        if df.empty:
+            raise Exception('Empty Dataframe')
+        else:
+            df.columns = df.columns.map(lambda x: x.replace(' ', '_').lower())
 
-        return df
+            return df
